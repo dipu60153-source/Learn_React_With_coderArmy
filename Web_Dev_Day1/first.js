2fc07720f6b1c8d8b5918208d20b2f4da3901630
Dipu kumar <kumardipu845420@gmail.com>
2025-08-31 15:07:48 +0530

    first commit

---
+++ Web_Dev_Day1/first.js
@@ -0,0 +1,54 @@
+
+//create element through js
+// style ={fontSize:"30px",backgroundColor:"red",color:'white'}
+
+// const  createElement  = require("react");
+
+const React = {
+    createElement:function(tag,style,children){
+     const element= document.createElement(tag);
+     element.innerText=children;
+
+     for(let key in style){
+        element.style[key]=style[key];
+     }
+     return element;
+    }
+}
+
+const ReactDom ={
+    render:function(element,root){
+        root.append(element);
+    }
+}
+
+const header1=React.createElement('h1',{fontSize:"30px",backgroundColor:"red",color:'white'},"hello coder army");
+
+const header2=React.createElement('h2',{fontSize:"30px",backgroundColor:"blue",color:'white'},"coding shikho");
+
+
+
+// const header = document.createElement('h1');
+// header.innerText="hello coder army";
+// header.style.backgroundColor="pink";
+// header.style.fontSize="30px";
+// header.style.color="red";
+
+
+// const root = document.getElementById("root");
+// root.append(header1);
+// root.append(header2)
+
+ReactDom.render(header1,document.getElementById("root"));
+
+ReactDom.render(header2,document.getElementById("root"));
+
+// const header2 = document.createElement('h1');
+// header2.innerText="coding";
+// header2.style.backgroundColor="red";
+// header2.style.fontSize="30px";
+// header2.style.color="black";
+
+// const root=document.getElementById('root');
+// root.append(header);
+// root.append(header2)
---
+++ Web_Dev_Day2/app.js
@@ -0,0 +1,9 @@
+const element=React.createElement("h1",{},"hello coder army");
+
+// document.getElementById("root");
+// ReactDom.render(element,document.getElementById("root"));
+
+const root=ReactDom.createRoot(document.getElementById('root'));
+
+
+root.render(element);
